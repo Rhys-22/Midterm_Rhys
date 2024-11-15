e4a072e4456966e3818e3a9c13211870b436925a
Rhys-22 <steveyhadz@gmail.com>
2024-11-16 03:41:59 +0800

    final fix of index and dashboard

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-
+if (isset($_SESSION['email'])) {
+    // If logged in, redirect to dashboard
+    header('Location: dashboard.php');
+    exit;
+}
 // Initialize error messages
 $emailError = "";
 $passwordError = "";
